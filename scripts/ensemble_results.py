da9bd1fb04f32d2b1916e8603077a0583b03ad01
connorcleary <theconnorac@gmail.com>
2023-03-28 12:59:22 +1300

    update linux

--- scripts/ensemble_results.py
+++ scripts/ensemble_results.py
@@ -12,16 +12,16 @@ def plot_metrics(name, times, axs2, axs3, axs4, axs5, axs3twin, axs4twin, axs5tw
     df = pd.read_csv(f"/home/ccleary/shared/results/{name}/metrics.csv")
     
     # plot fresh
-    axs2.plot(times[0::2], df.fresh_list)
-    axs3twin.plot(times[0::2], df.toe_list)
-    axs3twin.plot(times[0::2], df.tip_list)
-    axs4.plot(times[0::2], df.volume_list)
-    axs4twin.plot(times[0::2], df.centroid_list, linestyle="--")
-    axs5.plot(times[0::2], df.fresh_flux_list)
-    axs5.plot(times[0::2], df.sal_flux_list)
+    axs2.plot(times[0::5], df.fresh_list)
+    axs3twin.plot(times[0::5], df.toe_list)
+    axs3twin.plot(times[0::5], df.tip_list)
+    axs4.plot(times[0::5], df.volume_list)
+    axs4twin.plot(times[0::5], df.centroid_list, linestyle="--")
+    axs5.plot(times[0::5], df.fresh_flux_list)
+    axs5.plot(times[0::5], df.sal_flux_list)
     axs5.set_xlabel("time from present (days)")
-    axs5twin.plot(times[0::2], df.fresh_centroid_list, linestyle="--")
-    axs5twin.plot(times[0::2], df.sal_centroid_list, linestyle="--")
+    axs5twin.plot(times[0::5], df.fresh_centroid_list, linestyle="--")
+    axs5twin.plot(times[0::5], df.sal_centroid_list, linestyle="--")
 
 
     if y_labels == "left":
@@ -109,18 +109,15 @@ def plot_set(inputs):
     
     d = {}
 
-    i = 0
-    for r in set:
-        if i >= mid-1:
-           i=i+1
+    for i in range(len(set)+1):
         d[f"axs3_{i}twin"] = axs[3][i].twinx()
         d[f"axs4_{i}twin"] = axs[4][i].twinx()
         d[f"axs5_{i}twin"] = axs[5][i].twinx()
-    
+
     conc, _, qx, qz, times = get_results("a0")
     axs[0][mid], axs[1][mid] = plot_results("a0", conc, qx, qz, times, axs[0][mid], axs[1][mid])
     axs[2][mid], axs[3][mid], axs[4][mid], axs[5][mid], d[f"axs3_{mid}twin"], d[f"axs5_{mid}twin"],d[f"axs5_{mid}twin"] \
-        = plot_metrics("a0", times, axs[2][mid], axs[3][mid], axs[4][mid], axs[5][mid], d[f"axs3_{mid}twin"], d[f"axs5_{mid}twin"],d[f"axs5_{mid}twin"])
+        = plot_metrics("a0", times, axs[2][mid], axs[3][mid], axs[4][mid], axs[5][mid], d[f"axs3_{mid}twin"], d[f"axs4_{mid}twin"],d[f"axs5_{mid}twin"])
 
     i = 0
     y_labels= "left"
@@ -130,8 +127,8 @@ def plot_set(inputs):
         conc, _, qx, qz, times = get_results(r)
         # mf6r.metrics(r, conc, qz)
         axs[0][i], axs[1][i] = plot_results(r, conc, qx, qz, times, axs[0][i], axs[1][i], y_labels=y_labels)
-        axs[2][i], axs[3][i], axs[4][i], axs[5][i], d[f"axs3_{i}twin"], d[f"axs5_{i}twin"],d[f"axs5_{i}twin"]  \
-            = plot_metrics(r, times, axs[2][i], axs[3][i], axs[4][i], axs[5][i], d[f"axs3_{i}twin"], d[f"axs5_{i}twin"],d[f"axs5_{i}twin"], y_labels=y_labels)
+        axs[2][i], axs[3][i], axs[4][i], axs[5][i], d[f"axs3_{i}twin"], d[f"axs4_{i}twin"],d[f"axs5_{i}twin"]  \
+            = plot_metrics(r, times, axs[2][i], axs[3][i], axs[4][i], axs[5][i], d[f"axs3_{i}twin"], d[f"axs4_{i}twin"],d[f"axs5_{i}twin"], y_labels=y_labels)
         i=i+1
         if i < len(set)-2:
             y_labels="none"
@@ -152,10 +149,10 @@ def plot_ensemble(sets, set_names, name):
     conc0p, conc0m = mf6r.plot_results("a0", conc, qx, qz, times, plot=False)
     fresh0, toe0, mix0, sgd0, toe0twin, mix0twin, sgd0twin = mf6r.plot_metrics("a0", times, plot=False)
     base = [conc0p, conc0m, fresh0, toe0, mix0, sgd0, toe0twin, mix0twin, sgd0twin]
-    # plot_set(sets[0], set_names[0], base)
-    inputs = [[s, n] for s, n in zip(sets, set_names)]
-    p = Pool(processes=16)
-    p.map(plot_set, inputs)
+    plot_set([sets[0], set_names[0]])
+    # inputs = [[s, n] for s, n in zip(sets, set_names)]
+    # p = Pool(processes=16)
+    # p.map(plot_set, inputs)
 
 
 def main():
