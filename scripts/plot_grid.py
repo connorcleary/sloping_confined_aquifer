95f8e2874022b6149c27fdf64e38eae0c956980d
Connor Cleary <connorc204@gmail.com>
2024-08-20 11:23:11 +1200

    add yml

--- scripts/plot_grid.py
+++ scripts/plot_grid.py
@@ -13,7 +13,7 @@ def main():
    cs = flopy.plot.PlotCrossSection(model=swt.to_plot,ax= ax, line={"row":0})
    cs.plot_grid(lw=0.1, color="0.5")
    cs.plot_ibound()
-   x = [0, 3000, 8000,13000]
+   x = [0, 3000, 8000, 13000]
    xticks =[int(-3000), int(0), int(5000),int(10000)]
    plt.xticks(x, xticks)
    ax.set_xlabel("Distance offshore [m]")
